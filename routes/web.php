30d319c27c758c512ea624e145c71522d1601254
Franx21 <amvworld2015@gmail.com>
2024-01-13 20:30:48 -0700

    se agrego el login y la validacion de usuario

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,5 +23,6 @@ Route::get('/', function () {
 
 Route::get('/crear-cuenta', [RegisterController::class, 'crear'])->name('register');
 Route::post('/crear-cuenta', [RegisterController::class, 'store']);
-
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
 Route::get('/muro', [PostController::class, 'index'])->name('post.index');
