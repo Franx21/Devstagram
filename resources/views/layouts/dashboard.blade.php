244080f27ad9d660eecee762010eeaa4e366ea3c
Franx21 <amvworld2015@gmail.com>
2024-01-15 22:32:59 -0700

    se actualizo la informacion de perfil de los usuarios

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('titulo')
-    Tu Cuenta
+    Perfil: {{ $user->username }}
 @endsection
 
 @section('contenido')
@@ -10,8 +10,20 @@
             <div class="md:w-8/12 lg:w-8/12 px-5">
                 <img src="{{ asset('img/usuario.svg') }}" alt="imagen de usuario">
             </div>
-            <div class="md:w-8/12 lg:w-8/12 px-5">
-                <p class="text-gray-600 text-xl">{{ auth()->user()->username }}</p>
+            <div class="md:w-8/12 lg:w-8/12 px-5 flex flex-col md:justify-center items-center md:items-start py-10">
+                <p class="text-gray-600 text-xl">{{ $user->username }}</p>
+                <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
+                    0
+                    <span class="font-normal">Seguidores</span>
+                </p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0
+                    <span class="font-normal">Siguiendo</span>
+                </p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0
+                    <span class="font-normal">Posts</span>
+                </p>
             </div>
         </div>
     </div>
