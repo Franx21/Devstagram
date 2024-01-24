29341b26cb8d4f659391de33a890d1fba382fa7d
Franx21 <amvworld2015@gmail.com>
2024-01-24 01:19:45 -0700

    se agrego la subida de imagenes con dropzone

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -3,20 +3,24 @@
 @section('titulo')
     Crear una nueva Publicacion
 @endsection
-
+@push('styles')
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
 @section('contenido')
     <div class="md:flex md:items-center">
         <div class="md:w-1/2 px-10">
-            <form action="/IMAGENES" id="dropzone"
-                class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center"></form>
+            <form action="{{ route('imagen.store') }}" id="dropzone"
+                class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+                @csrf
+            </form>
         </div>
         <div class="md:w-1/2 px-10 bg-white rounded-lg mt-10 md:mt-0">
-            <form action="{{ route('register') }}" method="post" novalidate>
+            <form action="{{ route('register') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-5">
                     <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
                         Titulo
-                    </label>    
+                    </label>
                     <input id="titulo" name="titulo" type="text" placeholder="El titulo"
                         class="border p-3 w-full rounded-lg @error('titulo')
                                     border-red-500 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PostController;
@@ -29,3 +30,4 @@ Route::post('/login', [LoginController::class, 'store']);
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 Route::get('/{user:username}', [PostController::class, 'index'])->name('post.index');
 Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagen.store');
