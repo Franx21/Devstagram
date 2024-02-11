803a76995177a2d7c9a0871a2b6c917c79a8d80b
Franx21 <amvworld2015@gmail.com>
2024-02-11 14:00:05 -0700

    se agrego el botom like

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ComentarioController;
 use App\Http\Controllers\ImagenController;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PostController;
@@ -40,3 +41,5 @@ Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'stor
 
 
 Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
+
+Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
