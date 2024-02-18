12e7131ab4d710e009bd709740254d3e0a92b8b7
Franx21 <amvworld2015@gmail.com>
2024-02-18 15:12:30 -0700

    se agrego livewire en el botom like

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <title>DevStagram - @yield('titulo')</title>
     @stack('styles')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
 </head>
 
 <body class="bg-gray-100">
@@ -61,6 +62,7 @@
     <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
         DevStagram - Todos los derechos reservados {{ now()->year }}
     </footer>
+    @livewireScripts
 </body>
 
 </html>
